6c6df58563fd15cb747a40fab3c92a0855a1bee2
Sofia <sofia@sofia.local>
2017-07-05 20:57:47 -0400

    Updated index  video list

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -26,3 +26,5 @@ export default SearchBar;
 //constructor for setup: initializing variables etc.
 
 //  the biggest difference between the stateless functional and class components is how you manage state. When you use React.Component or React.createClass, you still have access to all the lifecycle methods and the internal state (this.state), so the expectation is that you'll manage state within the component. Functional components, on the other hand, just take an input (the props) and return an output (the html), so state needs to be managed elsewhere; you have to pass in any event handlers and application state that you need. Functional components are much easier to reason about, in my opinion, but they usually need to be paired with some state management library like redux. You can also use the two types together, using a stateful component to manage state and passing that down to stateless child components.
+
+// Downwards -- only the most parent component should be fetching data 
