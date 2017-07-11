44aa88883857010d449649bc1a3e6fcf7124b1ac
Sofia <sofia@sofia.local>
2017-07-10 22:29:50 -0400

    updated styling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,7 +7,7 @@ class SearchBar extends Component {
   }
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input
         value={this.state.term}
         onChange={event => this.setState({term: event.target.value })} />;
@@ -27,4 +27,4 @@ export default SearchBar;
 
 //  the biggest difference between the stateless functional and class components is how you manage state. When you use React.Component or React.createClass, you still have access to all the lifecycle methods and the internal state (this.state), so the expectation is that you'll manage state within the component. Functional components, on the other hand, just take an input (the props) and return an output (the html), so state needs to be managed elsewhere; you have to pass in any event handlers and application state that you need. Functional components are much easier to reason about, in my opinion, but they usually need to be paired with some state management library like redux. You can also use the two types together, using a stateful component to manage state and passing that down to stateless child components.
 
-// Downwards -- only the most parent component should be fetching data 
+// Downwards -- only the most parent component should be fetching data
--- src/index.js
+++ src/index.js
@@ -12,19 +12,40 @@
 // 1 component per file
 
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 const API_KEY = 'AIzaSyAvC6dKvqNUfAt0pFeo1Jqm78EuZMmSzKw';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-     </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
+
+    YTSearch({key: API_KEY, term: 'puppy'}, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+       });
+    });
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos}/>
+       </div>
+    );
+  }
 }
 
 // instantiate the component, before you pass it to the DOM
